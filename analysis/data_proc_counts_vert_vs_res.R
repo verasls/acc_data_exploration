c9194760800181d6f4a9b60454fdb588e3d37203
Lucas Veras <lucasdsveras@gmail.com>
2019-01-11 17:25:54 +0000

    Initial commit data processing script

---
+++ analysis/data_proc_counts_vert_vs_res.R
@@ -0,0 +1,6 @@
+source("R/calculateAccMetricsAvg.R")
+
+# Calculate average counts by speed ---------------------------------------
+
+calculateAccMetricsAvg("/Volumes/LV_HD/Accelerometry/PAI_ACC/projects/ECSS_2019/counts_back/output_30secROI")
+calculateAccMetricsAvg("/Volumes/LV_HD/Accelerometry/PAI_ACC/projects/ECSS_2019/counts_hip/output_30secROI")
