b7e59acefc4a52c2a832c618ab16915abc2f51ef
Lucas Veras <lucasdsveras@gmail.com>
2019-01-14 12:43:41 +0000

    Delete file

--- analysis/data_proc_counts_vert_vs_res.R
+++
@@ -1,6 +0,0 @@
-source("R/calculateAccMetricsAvg.R")
-
-# Calculate average counts by speed ---------------------------------------
-
-calculateAccMetricsAvg("/Volumes/LV_HD/Accelerometry/PAI_ACC/projects/ECSS_2019/counts_back/output_30secROI")
-calculateAccMetricsAvg("/Volumes/LV_HD/Accelerometry/PAI_ACC/projects/ECSS_2019/counts_hip/output_30secROI")
