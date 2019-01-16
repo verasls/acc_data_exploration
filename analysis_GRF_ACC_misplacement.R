8a349ca6d073d14d0980a00d5649980a879025ae
Lucas Veras <lucasdsveras@gmail.com>
2019-01-16 12:09:47 +0000

    include mixed model analysis section

--- analysis_GRF_ACC_misplacement.R
+++ analysis_GRF_ACC_misplacement.R
@@ -1,6 +1,7 @@
 library(tidyverse)
-library(nlme)
 library(plyr)
+library(nlme)
+library(multcomp)
 library(ggplot2)
 source("R/cvLR.R")
 source("R/accuracyIndices.R")
@@ -9,11 +10,11 @@ source("R/accuracyIndices.R")
 
 back <- read.csv("data/GRF_ACC_misplacement/Back_sec.csv") %>%
   as.tibble() %>% 
-  select(-name)
+  dplyr::select(-name)
 
 hip <- read.csv("data/GRF_ACC_misplacement/Hip_sec.csv") %>%
   as.tibble() %>% 
-  select(-name)
+  dplyr::select(-name)
 
 # Data analysis -----------------------------------------------------------
 
@@ -33,61 +34,167 @@ back.data.by.hip.LR <- cbind(back, predict(hip.LR, newdata = back, level = 0)) %
   as.tibble()
 hip.data.by.back.LR <- cbind(hip, predict(back.LR, newdata = hip, level = 0)) %>% 
   as.tibble()
-names(back.data.by.hip.LR)[8] <- "back.data.by.hip.LR"
-names(hip.data.by.back.LR)[8] <- "hip.data.by.back.LR"
+names(back.data.by.hip.LR)[8] <- "back_ACC_by_hip_LR"
+names(hip.data.by.back.LR)[8] <- "hip_ACC_by_back_LR"
 
 # ** Indices of accuracy --------------------------------------------------
 
 back.accuracy        <- accuracyIndices(back.LOOCV, "pRGRF_N_predicted", "pRGRF_N")
 hip.accuracy         <- accuracyIndices(hip.LOOCV, "pRGRF_N_predicted", "pRGRF_N")
-back.by.hip.accuracy <- accuracyIndices(back.data.by.hip.LR, "back.data.by.hip.LR", "pRGRF_N")
-hip.by.back.accuracy <- accuracyIndices(hip.data.by.back.LR, "hip.data.by.back.LR", "pRGRF_N")
+back.by.hip.accuracy <- accuracyIndices(back.data.by.hip.LR, "back_ACC_by_hip_LR", "pRGRF_N")
+hip.by.back.accuracy <- accuracyIndices(hip.data.by.back.LR, "hip_ACC_by_back_LR", "pRGRF_N")
 
 # ** Mixed model ----------------------------------------------------------
 
-
 # **** Building dataframe -------------------------------------------------
-
+# Get predicted pRGRF values
 back.by.back <- back.LOOCV %>% 
-  select(ID, speed, pRGRF_N, pRGRF_N_predicted)
+  dplyr::select(ID, speed, pRGRF_N_predicted)
 hip.by.hip <- hip.LOOCV %>% 
-  select(ID, speed, pRGRF_N_predicted)
+  dplyr::select(ID, speed, pRGRF_N_predicted)
 back.by.hip <- back.data.by.hip.LR %>% 
-  select(ID, speed, back.data.by.hip.LR)
+  dplyr::select(ID, speed, back_ACC_by_hip_LR)
 hip.by.back <- hip.data.by.back.LR %>% 
-  select(ID, speed, hip.data.by.back.LR)
+  dplyr::select(ID, speed, hip_ACC_by_back_LR)
 
-names(back.by.back)[4] <- "pRGRF_N_back_by_back"
-names(hip.by.hip)[3]   <- "pRGRF_N_hip_by_hip"
-names(back.by.hip)[3]  <- "pRGRF_N_back_by_hip"
-names(hip.by.back)[3]  <- "pRGRF_N_hip_by_back"
+names(back.by.back)[3] <- "back_ACC_by_back_LR"
+names(hip.by.hip)[3]   <- "hip_ACC_by_hip_LR"
 
-all.predicted <- join_all(
-  list(back.by.back, hip.by.hip, back.by.hip, hip.by.back), 
-  by = c("ID", "speed"), 
-  type = "left"
-  ) %>% 
-  as.tibble() %>% 
+back.by.back <- back.by.back %>% 
+  spread(key = speed, value = back_ACC_by_back_LR) %>% 
+  na.omit() %>% 
   gather(
-    pRGRF_N, pRGRF_N_back_by_back, pRGRF_N_hip_by_hip, pRGRF_N_back_by_hip, pRGRF_N_hip_by_back,
-    key = "valor",
-    value = "by"
+    `2`, `3`, `4`, `5`, `6`,
+    key = speed,
+    value = back_ACC_by_back_LR
   )
 
+hip.by.hip <- hip.by.hip %>% 
+  spread(key = speed, value = hip_ACC_by_hip_LR) %>% 
+  na.omit() %>% 
+  gather(
+    `2`, `3`, `4`, `5`, `6`,
+    key = speed,
+    value = hip_ACC_by_hip_LR
+  )
+
+back.by.hip <- back.by.hip %>% 
+  spread(key = speed, value = back_ACC_by_hip_LR) %>% 
+  na.omit() %>% 
+  gather(
+    `2`, `3`, `4`, `5`, `6`,
+    key = speed,
+    value = back_ACC_by_hip_LR
+  )
+
+hip.by.back <- hip.by.back %>%
+  spread(key = speed, value = hip_ACC_by_back_LR) %>%
+  na.omit() %>%
+  gather(
+    `2`, `3`, `4`, `5`, `6`,
+    key = speed,
+    value = hip_ACC_by_back_LR
+  )
+
+# Get measured pRGRF value as average of back and hip
+back.measured.pRGRF <- back %>% 
+  dplyr::select(c(ID, speed, pRGRF_N)) %>% 
+  spread(key = speed, value = pRGRF_N) %>% 
+  na.omit() %>% 
+  gather(
+    `2`, `3`, `4`, `5`, `6`,
+    key = speed,
+    value = back_measured_pRGRF
+  )
+
+hip.measured.pRGRF <- hip %>% 
+  dplyr::select(c(ID, speed, pRGRF_N)) %>% 
+  spread(key = speed, value = pRGRF_N) %>% 
+  na.omit() %>% 
+  gather(
+    `2`, `3`, `4`, `5`, `6`,
+    key = speed,
+    value = hip_measured_pRGRF
+  )
+
+measured.pRGRF <- back.measured.pRGRF %>% 
+  left_join(hip.measured.pRGRF, by = c("ID", "speed"))
+
+measured.pRGRF$measured <- NA
+for (i in 1:nrow(measured.pRGRF)) {
+  measured.pRGRF$measured[i] <- mean(
+    c(
+      measured.pRGRF$back_measured_pRGRF[i], 
+      measured.pRGRF$hip_measured_pRGRF[i]
+    )
+  )
+}
+
+measured.pRGRF <- dplyr::select(measured.pRGRF, c(ID, speed, measured))
+
+# Merge all dataframes
+all.pRGRF <- join_all(
+  list(measured.pRGRF, back.by.back, hip.by.hip, back.by.hip, hip.by.back),
+  by = c("ID", "speed"),
+  type = "left"
+) %>% 
+  as.tibble() %>% 
+  gather(
+    measured, back_ACC_by_back_LR, hip_ACC_by_hip_LR, back_ACC_by_hip_LR, hip_ACC_by_back_LR,
+    key = "group",
+    value = "pRGRF"
+  )
+all.pRGRF$speed <- as.numeric(all.pRGRF$speed)
+all.pRGRF$group <- as.factor(all.pRGRF$group)
+
 # **** Mixed model analysis -----------------------------------------------
-pd <- position_dodge(0.5)
 
-ggplot(data = all.predicted, aes(x = speed, y = by, colour = valor)) + 
-  stat_summary(fun.y = mean, geom = "point", position = pd) +
-  stat_summary(fun.y = mean, geom = "line", position = pd) +
-  stat_summary(fun.data = mean_cl_normal, geom = "errorbar", width = 0.4, position = pd)
-  
-ggplot(data = back.LOOCV, aes(x = speed, y = pRGRF_N)) + 
-  stat_summary(fun.y = mean, geom = "point", position = pd) +
-  stat_summary(fun.y = mean, geom = "line", position = pd) +
-  stat_summary(fun.data = mean_cl_normal, geom = "errorbar", width = 0.4, position = pd)
+group.model <- lme(
+  fixed = pRGRF ~ group,
+  random = ~ 1 | ID/speed/group,
+  method = "ML",
+  data = all.pRGRF
+)
+summary(group.model)
 
-ggplot(data = ANOVA_R, aes(Speed, GRF, colour = Code_Factor)) + 
-  stat_summary(fun.y = mean, geom = "point", position = pd) +
-  stat_summary(fun.y = mean, geom = "line", position = pd) +
-  stat_summary(fun.data = mean_cl_normal, geom = "errorbar", width = 0.4, position = pd)
+baseline <- lme(
+  fixed = pRGRF ~ 1,
+  random = ~ 1 | ID/speed/group,
+  method = "ML",
+  data = all.pRGRF
+)
+summary(baseline)
+
+anova(baseline, group.model)
+
+posthoc <- glht(model = group.model, linfct = mcp(group = "Tukey"))
+summary(posthoc)
+
+# ** Graph ----------------------------------------------------------------
+
+pRGRF_by_group <- ggplot(data = all.pRGRF, aes(x = speed, y = pRGRF, colour = group)) + 
+  stat_summary(fun.y = mean, geom = "point", position = position_dodge(0.5)) +
+  stat_summary(fun.y = mean, geom = "line", position = position_dodge(0.5)) +
+  stat_summary(fun.data = mean_cl_normal, geom = "errorbar", width = 0.4, position = position_dodge(0.5)) +
+  theme_bw() +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank(),
+    axis.line = element_line(size = 0.5, colour = "Black"),
+    axis.ticks = element_line(size = 0.5, colour = "Black"),
+    axis.ticks.length = unit(2, "mm"),
+    plot.title = element_text(hjust = 0.5)
+  ) +
+  scale_y_continuous(breaks = seq(from = 800, to = 1500, by = 100)) +
+  expand_limits(y = c(800, 1500))
+
+
+# ** pRGRF means by group -------------------------------------------------
+
+pRGRF_mean_by_group <- all.pRGRF %>% 
+  group_by(group, speed) %>% 
+  dplyr::summarise(mean = mean(pRGRF)) %>% 
+  spread(key = group, value = mean)
+# Reorder
+pRGRF_mean_by_group <- pRGRF_mean_by_group[, c(1, 6, 2, 5, 3, 4)]
