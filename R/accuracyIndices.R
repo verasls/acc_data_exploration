b6c0c825b76cbbf8b059d757fb8c68d75fc7c805
Lucas Veras <lucasdsveras@gmail.com>
2019-01-20 11:57:34 +0000

    Correct MAPE computation

--- R/accuracyIndices.R
+++ R/accuracyIndices.R
@@ -12,21 +12,21 @@ accuracyIndices <- function(df, predicted.var, dependent.var) {
   df <- as.data.frame(df)
   
   # Absolute values
-  bias.mean <- round(mean(df[ , predicted.var] - df[ , dependent.var]), digits = 2)
-  bias.sd   <- round(sd(df[ , predicted.var] - df[ , dependent.var]), digits = 2)
+  bias.mean <- round(mean(df[ , dependent.var] - df[ , predicted.var]), digits = 2)
+  bias.sd   <- round(sd(df[ , dependent.var] - df[ , predicted.var]), digits = 2)
   LoA.inf   <- round(bias.mean - (1.96 * bias.sd), digits = 2)
   LoA.sup   <- round(bias.mean + (1.96 * bias.sd), digits = 2)
-  MAE       <- round(mean(abs(df[ , predicted.var] - df[ , dependent.var])), digits = 2)
-  MAE.sd    <- round(sd(abs(df[ , predicted.var] - df[ , dependent.var])), digits = 2)
-  RMSE      <- round(sqrt(mean((df[ , predicted.var] - df[ , dependent.var])^2)), digits = 2)
+  MAE       <- round(mean(abs(df[ , dependent.var] - df[ , predicted.var])), digits = 2)
+  MAE.sd    <- round(sd(abs(df[ , dependent.var] - df[ , predicted.var])), digits = 2)
+  RMSE      <- round(sqrt(mean((df[ , dependent.var] - df[ , predicted.var])^2)), digits = 2)
   
   # Percent values
-  bias.mean.p <- round(mean(((df[ , predicted.var] - df[ , dependent.var]) / df[ , predicted.var])) * 100, digits = 2)
-  bias.sd.p   <- round(sd(((df[ , predicted.var] - df[ , dependent.var]) / df[ , predicted.var])) * 100, digits = 2)
+  bias.mean.p <- round(mean(((df[ , dependent.var] - df[ , predicted.var]) / df[ , dependent.var])) * 100, digits = 2)
+  bias.sd.p   <- round(sd(((df[ , dependent.var] - df[ , predicted.var]) / df[ , dependent.var])) * 100, digits = 2)
   LoA.inf.p   <- round(bias.mean.p - (1.96 * bias.sd.p), digits = 2)
   LoA.sup.p   <- round(bias.mean.p + (1.96 * bias.sd.p), digits = 2)
-  MAPE        <- round(mean(abs((df[ , predicted.var] - df[ , dependent.var]) / df[ , predicted.var])) * 100, digits = 2)
-  MAPE.sd     <- round(sd(abs((df[ , predicted.var] - df[ , dependent.var]) / df[ , predicted.var])) * 100, digits = 2)
+  MAPE        <- round(mean(abs((df[ , dependent.var] - df[ , predicted.var]) / df[ , dependent.var])) * 100, digits = 2)
+  MAPE.sd     <- round(sd(abs((df[ , dependent.var] - df[ , predicted.var]) / df[ , dependent.var])) * 100, digits = 2)
   CV.RMSE     <- round((RMSE / mean(df[ , dependent.var])) * 100, digits = 2)
   
   accuracy <- data.frame(
